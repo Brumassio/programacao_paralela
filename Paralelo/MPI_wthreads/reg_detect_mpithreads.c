02cf342246eb5b957d0c2b0a034ecb5c8d10bcf7
Brumassio <dibrunspeed@gmail.com>
2023-10-05 15:16:04 -0300

    funcionando, mas demorando pra rodar ya

--- Paralelo/MPI_wthreads/reg_detect_mpithreads.c
+++ Paralelo/MPI_wthreads/reg_detect_mpithreads.c
@@ -2,16 +2,31 @@
 #include <string.h>
 #include <math.h>
 #include <mpi.h>
-#include <omp.h>
+#include <pthread.h>
 
 #include "polybench.h"
 #include "reg_detect.h"
 
-static
-void init_array(int maxgrid,
-                DATA_TYPE POLYBENCH_2D(sum_tang, MAXGRID, MAXGRID, maxgrid, maxgrid),
-                DATA_TYPE POLYBENCH_2D(mean, MAXGRID, MAXGRID, maxgrid, maxgrid),
-                DATA_TYPE POLYBENCH_2D(path, MAXGRID, MAXGRID, maxgrid, maxgrid))
+#define NUM_THREADS 4
+
+pthread_barrier_t barr;
+pthread_mutex_t lock;
+
+typedef struct {
+    int niter;
+    int maxgrid;
+    int length;
+    DATA_TYPE (*sum_tang)[MAXGRID];
+    DATA_TYPE (*mean)[MAXGRID];
+    DATA_TYPE (*path)[MAXGRID];
+    DATA_TYPE (*diff)[MAXGRID][LENGTH];
+    DATA_TYPE (*sum_diff)[MAXGRID][LENGTH];
+} ThreadArgs;
+
+static void init_array(int maxgrid,
+                       DATA_TYPE POLYBENCH_2D(sum_tang, MAXGRID, MAXGRID, maxgrid, maxgrid),
+                       DATA_TYPE POLYBENCH_2D(mean, MAXGRID, MAXGRID, maxgrid, maxgrid),
+                       DATA_TYPE POLYBENCH_2D(path, MAXGRID, MAXGRID, maxgrid, maxgrid))
 {
     int i, j;
 
@@ -23,64 +38,64 @@ void init_array(int maxgrid,
         }
 }
 
-static
-void print_array(int maxgrid,
-                 DATA_TYPE POLYBENCH_2D(path, MAXGRID, MAXGRID, maxgrid, maxgrid))
+static void print_array(int maxgrid,
+                        DATA_TYPE POLYBENCH_2D(path, MAXGRID, MAXGRID, maxgrid, maxgrid))
 {
     int i, j;
 
     for (i = 0; i < maxgrid; i++)
         for (j = 0; j < maxgrid; j++) {
             fprintf(stderr, DATA_PRINTF_MODIFIER, path[i][j]);
-            if ((i * maxgrid + j) % 20 == 0) fprintf(stderr, "\n");
+            if ((i * maxgrid + j) % 20 == 0)
+                fprintf(stderr, "\n");
         }
     fprintf(stderr, "\n");
 }
 
-static
-void kernel_reg_detect(int niter, int maxgrid, int length,
-                       DATA_TYPE POLYBENCH_2D(sum_tang, MAXGRID, MAXGRID, maxgrid, maxgrid),
-                       DATA_TYPE POLYBENCH_2D(mean, MAXGRID, MAXGRID, maxgrid, maxgrid),
-                       DATA_TYPE POLYBENCH_2D(path, MAXGRID, MAXGRID, maxgrid, maxgrid),
-                       DATA_TYPE POLYBENCH_3D(diff, MAXGRID, MAXGRID, LENGTH, maxgrid, maxgrid, length),
-                       DATA_TYPE POLYBENCH_3D(sum_diff, MAXGRID, MAXGRID, LENGTH, maxgrid, maxgrid, length),
-                       int start_iter, int end_iter)
-{
+static void *thread_kernel(void *args) {
+    ThreadArgs *thread_args = (ThreadArgs *)args;
+
     int t, i, j, cnt;
+    int length = thread_args->length;
 
-    for (t = start_iter; t < end_iter; t++) {
-        for (j = 0; j < maxgrid; j++) {
-            for (i = j; i < maxgrid; i++) {
-                for (cnt = 0; cnt < _PB_LENGTH; cnt++)
-                    diff[j][i][cnt] = sum_tang[j][i];
+    for (t = 0; t < thread_args->niter; t++) {
+        for (j = 0; j < thread_args->maxgrid; j++) {
+            for (i = j; i < thread_args->maxgrid; i++) {
+                for (cnt = 0; cnt < length; cnt++)
+                    thread_args->diff[j][i][cnt] = thread_args->sum_tang[j][i];
             }
         }
 
-        #pragma omp parallel for collapse(3)
-        for (j = 0; j < maxgrid; j++) {
-            for (i = j; i < maxgrid; i++) {
-                sum_diff[j][i][0] = diff[j][i][0];
-                for (cnt = 1; cnt < _PB_LENGTH; cnt++)
-                    sum_diff[j][i][cnt] = sum_diff[j][i][cnt - 1] + diff[j][i][cnt];
-                mean[j][i] = sum_diff[j][i][_PB_LENGTH - 1];
+        pthread_barrier_wait(&barr);
+
+        for (j = 0; j < thread_args->maxgrid; j++) {
+            for (i = j; i < thread_args->maxgrid; i++) {
+                thread_args->sum_diff[j][i][0] = thread_args->diff[j][i][0];
+                for (cnt = 1; cnt < length; cnt++)
+                    thread_args->sum_diff[j][i][cnt] =
+                        thread_args->sum_diff[j][i][cnt - 1] + thread_args->diff[j][i][cnt];
+                thread_args->mean[j][i] = thread_args->sum_diff[j][i][length - 1];
             }
         }
 
-        #pragma omp parallel for
-        for (i = 0; i < maxgrid; i++)
-            path[0][i] = mean[0][i];
+        pthread_barrier_wait(&barr);
 
-        #pragma omp parallel for collapse(2)
-        for (j = 1; j < maxgrid; j++) {
-            for (i = j; i < maxgrid; i++) {
-                path[j][i] = path[j - 1][i - 1] + mean[j][i];
+        for (i = 0; i < thread_args->maxgrid; i++)
+            thread_args->path[0][i] = thread_args->mean[0][i];
+
+        for (j = 1; j < thread_args->maxgrid; j++) {
+            for (i = j; i < thread_args->maxgrid; i++) {
+                thread_args->path[j][i] = thread_args->path[j - 1][i - 1] + thread_args->mean[j][i];
             }
         }
+
+        pthread_barrier_wait(&barr);
     }
+
+    return NULL;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
 
     int rank, size;
@@ -91,11 +106,8 @@ int main(int argc, char **argv)
     int maxgrid = MAXGRID;
     int length = LENGTH;
 
-    int work_per_process = niter / size;
-    int extra_work = niter % size;
-
-    int start_iter = rank * work_per_process + ((rank < extra_work) ? rank : extra_work);
-    int end_iter = start_iter + work_per_process + ((rank < extra_work) ? 1 : 0);
+    int work_per_thread = niter / NUM_THREADS;
+    int extra_work = niter % NUM_THREADS;
 
     POLYBENCH_2D_ARRAY_DECL(sum_tang, DATA_TYPE, MAXGRID, MAXGRID, maxgrid, maxgrid);
     POLYBENCH_2D_ARRAY_DECL(mean, DATA_TYPE, MAXGRID, MAXGRID, maxgrid, maxgrid);
@@ -105,22 +117,40 @@ int main(int argc, char **argv)
 
     init_array(maxgrid, POLYBENCH_ARRAY(sum_tang), POLYBENCH_ARRAY(mean), POLYBENCH_ARRAY(path));
 
+    pthread_barrier_init(&barr, NULL, NUM_THREADS);
+    pthread_mutex_init(&lock, NULL);
+
+    pthread_t threads[NUM_THREADS];
+    ThreadArgs thread_args[NUM_THREADS];
     polybench_start_instruments;
 
-    kernel_reg_detect(niter, maxgrid, length,
-                      POLYBENCH_ARRAY(sum_tang),
-                      POLYBENCH_ARRAY(mean),
-                      POLYBENCH_ARRAY(path),
-                      POLYBENCH_ARRAY(diff),
-                      POLYBENCH_ARRAY(sum_diff),
-                      start_iter, end_iter);
+    for (int i = 0; i < NUM_THREADS; i++) {
+        thread_args[i].niter = work_per_thread + ((i < extra_work) ? 1 : 0);
+        thread_args[i].maxgrid = maxgrid;
+        thread_args[i].length = length;
+        thread_args[i].sum_tang = POLYBENCH_ARRAY(sum_tang);
+        thread_args[i].mean = POLYBENCH_ARRAY(mean);
+        thread_args[i].path = POLYBENCH_ARRAY(path);
+        thread_args[i].diff = POLYBENCH_ARRAY(diff);
+        thread_args[i].sum_diff = POLYBENCH_ARRAY(sum_diff);
+
+        pthread_create(&threads[i], NULL, thread_kernel, (void *)&thread_args[i]);
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    pthread_barrier_destroy(&barr);
+
+    pthread_mutex_destroy(&lock);
 
     MPI_Barrier(MPI_COMM_WORLD);
-
     polybench_stop_instruments;
-    polybench_print_instruments;
-
-    polybench_prevent_dce(print_array(maxgrid, POLYBENCH_ARRAY(path)));
+        polybench_print_instruments;
+    // if (rank == 0) {
+    //     print_array(maxgrid, POLYBENCH_ARRAY(path));
+    // }
 
     POLYBENCH_FREE_ARRAY(sum_tang);
     POLYBENCH_FREE_ARRAY(mean);
